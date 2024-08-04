f235dc8ced4efa63ae0441d4f76877ca57c8a59a
nao <>
2024-08-04 16:23:09 +0900

    敵の遠距離攻撃のアニメーションを追加

---
+++ Assets/Scripts/DestroyTime.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTime : MonoBehaviour
+{
+    public float leftTime;
+    void Start()
+    {
+        Destroy(gameObject,leftTime);
+    }
+
+    void Update()
+    {
+        
+    }
+}
