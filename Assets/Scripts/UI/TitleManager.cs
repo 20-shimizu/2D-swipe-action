bb32db416b57d533c76410fd807e73ca1af419c6
20-shimizu <reborn.titech@gmail.com>
2024-08-28 16:56:07 +0900

    クレジットダイアログ追加

--- Assets/Scripts/UI/TitleManager.cs
+++ Assets/Scripts/UI/TitleManager.cs
@@ -11,18 +11,30 @@ public class TitleManager : MonoBehaviour
     private float fadeOutSpeed = 255.0f;
     private Color32 fadeOutColor = new Color32(0, 0, 0, 0);
     private float alpha = 0.0f;
+    private Collider2D touchCollider;
+    private Button creditButton;
+    private GameObject creditDialog;
+    private Button creditCloseButton;
     // Start is called before the first frame update
     void Start()
     {
         audioManager = GameObject.Find("AudioSource").GetComponent<AudioManager>();
         darkCover = transform.Find("DarkCover").gameObject.GetComponent<Image>();
         darkCover.color = fadeOutColor;
+        touchCollider = transform.Find("TouchCollider").gameObject.GetComponent<Collider2D>();
+        creditButton = transform.Find("CreditButton").gameObject.GetComponent<Button>();
+        creditDialog = transform.Find("CreditDialog").gameObject;
+        creditCloseButton = creditDialog.transform.Find("CloseButton").gameObject.GetComponent<Button>();
+        creditDialog.SetActive(false);
+        creditButton.onClick.AddListener(OnClickCreditButton);
+        creditCloseButton.onClick.AddListener(OnClickCreditCloseButton);
+        darkCover.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && touchCollider.OverlapPoint(Input.mousePosition))
         {
             audioManager.PlaySE("ButtonPositive");
             StartCoroutine("FadeOut");
@@ -31,6 +43,7 @@ public class TitleManager : MonoBehaviour
 
     private IEnumerator FadeOut()
     {
+        darkCover.gameObject.SetActive(true);
         while (fadeOutColor.a < 250)
         {
             alpha += fadeOutSpeed * Time.deltaTime;
@@ -39,5 +52,18 @@ public class TitleManager : MonoBehaviour
             yield return null;
         }
         SceneManager.LoadScene("MapScene");
+    }
+
+    private void OnClickCreditButton()
+    {
+        audioManager.PlaySE("ButtonPositive");
+        creditDialog.SetActive(true);
+        touchCollider.gameObject.SetActive(false);
+    }
+    private void OnClickCreditCloseButton()
+    {
+        audioManager.PlaySE("ButtonNegative");
+        creditDialog.SetActive(false);
+        touchCollider.gameObject.SetActive(true);
     }
 }
