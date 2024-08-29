40d804852dceff2bd725441f75c965d1e0d754d5
Mutekichi <ppwatanabe19568@gmail.com>
2024-08-29 13:26:57 +0900

    マップ上のステージ入口のプレハブ作成

---
+++ Assets/Scripts/Map/StageEntranceController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class StageEntranceController : MonoBehaviour
+{
+    public string stageName;
+    public Sprite bossIcon;
+
+    private SpriteRenderer bossIconRenderer;
+    private SpriteRenderer capsuleRenderer;
+
+    private void Awake()
+    {
+        FindRenderers();
+    }
+
+    private void Start()
+    {
+        UpdateBossIcon();
+    }
+
+    private void OnValidate()
+    {
+        FindRenderers();
+        UpdateBossIcon();
+    }
+
+    private void FindRenderers()
+    {
+        // Capsuleのレンダラーを取得
+        Transform capsuleTransform = transform.Find("Capsule");
+        if (capsuleTransform != null)
+        {
+            capsuleRenderer = capsuleTransform.GetComponent<SpriteRenderer>();
+        }
+
+        // BossIconのレンダラーを取得
+        Transform bossIconTransform = transform.Find("Capsule/BossIcon");
+        if (bossIconTransform != null)
+        {
+            bossIconRenderer = bossIconTransform.GetComponent<SpriteRenderer>();
+        }
+    }
+
+    private void UpdateBossIcon()
+    {
+        if (bossIconRenderer != null)
+        {
+            bossIconRenderer.sprite = bossIcon;
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(bossIconRenderer);
+#endif
+        }
+        // Capsuleのスプライトは変更しない
+#if UNITY_EDITOR
+        SceneView.RepaintAll();
+#endif
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter");
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(stageName);
+        }
+    }
+}
